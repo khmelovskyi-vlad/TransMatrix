ac929d4a13941d0814f7af950b0356b69a44d786
vladhmelevskiy1234765 <vladhmelevskiy1234765@gmail.com>
2019-06-13 14:20:47 +0300

    mb end

--- TransMatrix/TransMatrix/Program.cs
+++ TransMatrix/TransMatrix/Program.cs
@@ -10,36 +10,9 @@ namespace TransMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Transpose matrices");
-            var widht = Enter("matrix weiht");
-            var height = Enter("matrix height");
-            int[,] matrix = new int[widht, height];
-            while (true)
-            {
-                try
-                {
-                    
-                    for (int i = 0; i < matrix.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < matrix.GetLength(1); j++)
-                        {
-                            var key = Console.ReadKey();
-                            if (key.Key == ConsoleKey.Escape)
-                            {
-                                throw new OperationCanceledException();
-                            }
-                            var line = Console.ReadLine();
-                            var keyLine = $"{key.KeyChar}{line}";
-                            matrix[i, j] = Convert.ToInt32(keyLine);
-                        }
-                    }
-                    break;
-                }
-                catch(FormatException ex)
-                {
-                    Console.WriteLine($"Bed input {ex.Message}, try again or click enter");
-                }
-            }
+            int width, height;
+            int[,] matrix;
+            Matrix(out matrix, out width, out height);
             Console.WriteLine("Your matrix is");
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
@@ -52,7 +25,7 @@ namespace TransMatrix
             Console.WriteLine("Your transponing matrix is");
             for (int i = 0; i < matrix.GetLength(1); i++)
             {
-                for(int j = 0; j < matrix.GetLength(0); j++)
+                for (int j = 0; j < matrix.GetLength(0); j++)
                 {
                     Console.Write(String.Format("{0,3}", matrix[j, i]));
                 }
@@ -60,7 +33,41 @@ namespace TransMatrix
             }
             Console.ReadKey();
         }
-        static int Enter (string ent)
+        static void Matrix(out int[,] mat, out int width, out int height)
+        {
+            Console.WriteLine("Transpose matrices");
+            width = Enter("matrix weiht");
+            height = Enter("matrix height");
+            mat = new int[width, height];
+            Console.WriteLine("Enter matrix");
+            while (true)
+            {
+                try
+                {
+
+                    for (int i = 0; i < mat.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < mat.GetLength(1); j++)
+                        {
+                            var key = Console.ReadKey();
+                            if (key.Key == ConsoleKey.Escape)
+                            {
+                                throw new OperationCanceledException();
+                            }
+                            var line = Console.ReadLine();
+                            var keyLine = $"{key.KeyChar}{line}";
+                            mat[i, j] = Convert.ToInt32(keyLine);
+                        }
+                    }
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"Bed input {ex.Message}, try again or click enter");
+                }
+            }
+        }
+        static int Enter(string ent)
         {
             do
             {
